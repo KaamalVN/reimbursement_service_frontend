5916818062f8d2975b488fb5ee1dd9f90dc69d58
KaamalVN <76166181+KaamalVN@users.noreply.github.com>
2024-12-24 16:40:49 +0530

    Update jwtUtils.js

--- src/utils/jwtUtils.js
+++ src/utils/jwtUtils.js
@@ -1,37 +1,45 @@
 import { jwtDecode } from 'jwt-decode'; // Corrected import
 
 export const getCompanyID = () => {
-    const token = localStorage.getItem('token');
-    if (token) {
-        const decoded = jwtDecode(token); // Decode the token
-        return decoded.company_id; // Retrieve company_id from the decoded payload
+    if (typeof window !== 'undefined') { // Check if window is defined
+        const token = localStorage.getItem('token');
+        if (token) {
+            const decoded = jwtDecode(token); // Decode the token
+            return decoded.company_id; // Retrieve company_id from the decoded payload
+        }
     }
-    return null; // Return null if no token exists
+    return null; // Return null if no token exists or window is not defined
 };
 
 export const getPermissionLevel = () => {
-    const token = localStorage.getItem('token');
-    if (token) {
-        const decoded = jwtDecode(token); // Decode the token
-        return decoded.permission_level; // Retrieve permission_level from the decoded payload
+    if (typeof window !== 'undefined') { // Check if window is defined
+        const token = localStorage.getItem('token');
+        if (token) {
+            const decoded = jwtDecode(token); // Decode the token
+            return decoded.permission_level; // Retrieve permission_level from the decoded payload
+        }
     }
-    return null; // Return null if no token exists
+    return null; // Return null if no token exists or window is not defined
 };
 
 export const getRoleID = () => {
-    const token = localStorage.getItem('token')  ;
-    if(token){
-        const decoded = jwtDecode(token);
-        return decoded.role_id;
+    if (typeof window !== 'undefined') { // Check if window is defined
+        const token = localStorage.getItem('token');
+        if (token) {
+            const decoded = jwtDecode(token);
+            return decoded.role_id; // Retrieve role_id from the decoded payload
+        }
     }
-    return null;
+    return null; // Return null if no token exists or window is not defined
 };
 
 export const getEmployeeID = () => {
-    const token = localStorage.getItem('token');
-    if (token) {
-        const decoded = jwtDecode(token);
-        return decoded.employee_id; // Access employee_id from the decoded token
+    if (typeof window !== 'undefined') { // Check if window is defined
+        const token = localStorage.getItem('token');
+        if (token) {
+            const decoded = jwtDecode(token); // Decode the token
+            return decoded.employee_id; // Access employee_id from the decoded token
+        }
     }
-    return null;
+    return null; // Return null if no token exists or window is not defined
 };
